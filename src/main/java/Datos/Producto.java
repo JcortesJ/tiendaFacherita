4811d25b9b282b372981a91d60265e0717579243
SimonRamos88 <joramos@unal.edu.co>
2021-12-27 11:35:58 -0500

    Inicio tienda

---
+++ src/main/java/Datos/Producto.java
@@ -0,0 +1,89 @@
+
+package Datos;
+
+
+public class Producto {
+    private int codigo;
+    private String nombre;
+    private String empresa;
+    private float precio;
+    private String descripcion;
+    
+    // SETTERS AND GETTERS
+    
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    // CONSTRUTORES
+    
+    public Producto(){
+        this(0, "Desconocido", "Desconocida", 0, "");
+    }
+    
+    public Producto(int codigo){
+        this(codigo, "Desconocido", "Desconocida", 0, "");
+    }
+    
+    public Producto(int codigo, String nombre){ 
+        this(codigo, nombre, "Desconocida", 0, "");
+    }
+    
+    public Producto(int codigo, String nombre, String empresa){
+        this(codigo, nombre, empresa, 0, "");
+    }
+    
+    public Producto(int codigo, String nombre, String empresa, float precio){
+        this(codigo, nombre, empresa, precio, "");
+    }
+    public Producto(int codigo, String nombre, String empresa, float precio, String desp){
+        this.codigo =  codigo;
+        this.nombre = nombre;
+        this.empresa= empresa;
+        this.precio = precio;
+        this.descripcion = desp;
+    }
+    
+    @Override
+    public String toString(){
+        String cad = "";
+        return cad;
+    }
+    
+}
---
+++ src/main/java/Logica/pruebas.java
@@ -0,0 +1,16 @@
+
+package Logica;
+
+import UI.*;
+import java.util.ArrayList;
+
+public class pruebas {
+    
+    public static void main(String[] arqs){
+        Menu interfaz = new Menu();    
+        interfaz.MostrarTipo("2");
+        ArrayList array = new ArrayList();
+        System.out.println(array.getClass());
+    }
+    
+}
