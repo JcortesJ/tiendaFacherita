aaf1d35a91b1dc84a04438241c4971d398cd2fb7
Jefferson <jeffersonduvanrami17@gmail.com>
2021-12-27 17:46:21 -0500

    hol

---
+++ src/main/java/Datos/comestibles/Calientes.java
@@ -0,0 +1,70 @@
+
+package Datos.comestibles;
+
+
+public class Calientes extends Bebida{
+    
+    private boolean tradicional;
+    private float nivelAzucar;
+    private boolean instantaneo;
+    
+    //GETTERS AND SETTERS
+
+    public boolean isTradicional() {
+        return tradicional;
+    }
+
+    public void setTradicional(boolean tradicional) {
+        this.tradicional = tradicional;
+    }
+
+    public float getNivelAzucar() {
+        return nivelAzucar;
+    }
+
+    public void setNivelAzucar(float nivelAzucar) {
+        this.nivelAzucar = nivelAzucar;
+    }
+
+    public boolean isInstantaneo() {
+        return instantaneo;
+    }
+
+    public void setInstantaneo(boolean instantaneo) {
+        this.instantaneo = instantaneo;
+    }
+    
+    //CONSTRUCTORES
+    
+    public Calientes(boolean tradicional, float nivelAzucar, boolean instantaneo,
+        float contenido, String empaquetado, boolean origenNatural,String vencimiento, 
+        float peso, int unidadesVenta, float calorias,int codigo, String nombre, 
+        String empresa, float precio, String desp){     
+        
+        super(contenido, empaquetado, origenNatural, vencimiento, peso, unidadesVenta, 
+                calorias, codigo, nombre, empresa, precio, desp);
+        this.tradicional = tradicional;
+        this.nivelAzucar = nivelAzucar;
+        this.instantaneo = instantaneo;
+    }
+    
+    public Calientes(boolean tradicional, float nivelAzucar, boolean instantaneo){          
+        super();
+        this.tradicional = tradicional;
+        this.nivelAzucar = nivelAzucar;
+        this.instantaneo = instantaneo;
+    }
+    
+    public Calientes(){
+        this(true,0,true);
+    }
+    
+    @Override
+    public String toString(){
+        String cad = super.toString();
+        cad += " Sabor Tradicional" + this.tradicional + "Nivel de azúcar: " + this.nivelAzucar+
+                "Bebida instantanea: " + this.instantaneo;
+        return cad;
+        
+    }
+}
---
+++ src/main/java/Datos/comestibles/Cerveza.java
@@ -0,0 +1,70 @@
+
+package Datos.comestibles;
+
+public class Cerveza extends Bebida{
+    
+    private float gradoAlcohol;
+    private String ingredientes;
+    private String tipoFermentacion;
+    
+    //GETTERS AND SETTERS
+
+    public float getGradoAlcohol() {
+        return gradoAlcohol;
+    }
+
+    public void setGradoAlcohol(float gradoAlcohol) {
+        this.gradoAlcohol = gradoAlcohol;
+    }
+
+    public String getIngredientes() {
+        return ingredientes;
+    }
+
+    public void setIngredientes(String ingredientes) {
+        this.ingredientes = ingredientes;
+    }
+
+    public String getTipoFermentacion() {
+        return tipoFermentacion;
+    }
+
+    public void setTipoFermentacion(String tipoFermentacion) {
+        this.tipoFermentacion = tipoFermentacion;
+    }
+    
+    //CONSTRUCTORES
+    
+    public Cerveza(float grado, String ingredientes, String fermentacion, 
+           float contenido, String empaquetado, boolean origenNatural,String vencimiento, 
+            float peso, int unidadesVenta, float calorias,int codigo, String nombre, 
+            String empresa, float precio, String desp){
+        
+        super(contenido, empaquetado, origenNatural, vencimiento, peso, unidadesVenta, 
+                calorias, codigo, nombre, empresa, precio, desp);
+        this.gradoAlcohol = grado;
+        this.ingredientes = ingredientes;
+        this.tipoFermentacion = fermentacion; 
+    }
+    
+    
+    public Cerveza(float grado, String ingredientes, String fermentacion){
+        super();
+        this.gradoAlcohol = grado;
+        this.ingredientes = ingredientes;
+        this.tipoFermentacion = fermentacion; 
+    }
+    
+    public Cerveza(){
+        this(0,"Desconocidos", "Desconocido");
+    }
+    
+    @Override
+    public String toString(){
+        String cad = super.toString();
+        cad += " Grado alcohol: " + this.gradoAlcohol + " Ingredientes: " + 
+                this.ingredientes + " Tipo de fermentación: "+ this.tipoFermentacion;
+        return cad;
+    }
+    
+}
