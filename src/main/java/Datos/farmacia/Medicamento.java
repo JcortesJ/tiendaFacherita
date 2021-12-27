351cff4a6132aa3799de01c69bcf659a0af4184f
juan <jcortesj@unal.edu.co>
2021-12-27 13:13:00 -0500

    no guarde xd

--- src/main/java/Datos/farmacia/Medicamento.java
+++ src/main/java/Datos/farmacia/Medicamento.java
@@ -3,13 +3,78 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
  */
 package Datos.farmacia;
+import Datos.*;
 
 /**
  *
  * @author juan manuel
  */
-public class Medicamento {
+public class Medicamento extends Farmacia {
 
+    public String getFechaVencimiento() {
+        return fechaVencimiento;
+    }
+
+    public void setFechaVencimiento(String fechaVencimiento) {
+        this.fechaVencimiento = fechaVencimiento;
+    }
+
+    public String getContraindicaciones() {
+        return contraindicaciones;
+    }
+
+    public void setContraindicaciones(String contraindicaciones) {
+        this.contraindicaciones = contraindicaciones;
+    }
+
+    public boolean isGenerica() {
+        return generica;
+    }
+
+    public void setGenerica(boolean generica) {
+        this.generica = generica;
+    }
+
+    public String getFrecuencia() {
+        return frecuencia;
+    }
+
+    public void setFrecuencia(String frecuencia) {
+        this.frecuencia = frecuencia;
+    }
+    
+    private String fechaVencimiento;
+    private String contraindicaciones;
+    private boolean generica;
+    private String frecuencia;
+
+    public Medicamento(String fechaVencimiento, String contraindicaciones, boolean generica, String frecuencia) {
+        this.fechaVencimiento = fechaVencimiento;
+        this.contraindicaciones = contraindicaciones;
+        this.generica = generica;
+        this.frecuencia = frecuencia;
+    }
+
+    public Medicamento(String fechaVencimiento, String contraindicaciones, boolean generica, String frecuencia, float contenido, String restr, String us0, int codigo, String nombre, String empresa, float precio, String desp) {
+        super(contenido, restr, us0, codigo, nombre, empresa, precio, desp);
+        this.fechaVencimiento = fechaVencimiento;
+        this.contraindicaciones = contraindicaciones;
+        this.generica = generica;
+        this.frecuencia = frecuencia;
+    }
+    
+       
+
+    @Override
+    public String toString() {
+        
+        return super.toString(); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
+    
+
+   
     /**
      * @param args the command line arguments
      */
