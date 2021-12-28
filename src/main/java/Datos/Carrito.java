968a3e44d4f937ed586d09f81a0911354424050d
SimonRamos88 <joramos@unal.edu.co>
2021-12-28 18:56:02 -0500

    metodos aniadir y eliminar al carrito

--- src/main/java/Datos/Carrito.java
+++ src/main/java/Datos/Carrito.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
- */
+
 package Datos;
 
 import java.util.HashMap;
 
-/**
- *
- * @author juan manuel
- */
+
 public class Carrito {
     
     private int cantidad;
     private Cliente user;
-    private HashMap<Producto, Integer> bolsa;
+    private HashMap<String, Integer> bolsa;
     
 
     public int getCantidad() {
@@ -33,21 +27,21 @@ public class Carrito {
         this.user = user;
     }
 
-    public HashMap<Producto, Integer> getBolsa() {
+    public HashMap<String, Integer> getBolsa() {
         return this.bolsa;
     }
 
-    public void setBolsa(HashMap<Producto,Integer> bolsa) {
+    public void setBolsa(HashMap<String,Integer> bolsa) {
         this.bolsa = bolsa;
     }
 
    
   
     public Carrito(){
-        this(0,new Cliente(),new HashMap<Producto, Integer>());
+        this(0,new Cliente(),new HashMap<String, Integer>());
     }
     
-    public Carrito(int cantidad, Cliente user, HashMap<Producto,Integer> bolsa){
+    public Carrito(int cantidad, Cliente user, HashMap<String,Integer> bolsa){
         this.cantidad = cantidad;
         this.user = user;
         this.bolsa = bolsa;
