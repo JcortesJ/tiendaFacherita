4396cf301d57b7e59429a0f70f8c2800160c8b39
Jefferson <jeffersonduvanrami17@gmail.com>
2021-12-28 18:50:51 -0500

    clase compra2

--- src/main/java/Datos/Compra.java
+++ src/main/java/Datos/Compra.java
@@ -1,13 +1,53 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package Datos;
 
-/**
- *
- * @author Jefferson
- */
 public class Compra {
+    private int codigo;
+    private String fecha;
+    private String factura;
+    private Cliente comprador;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getFactura() {
+        return factura;
+    }
+
+    public void setFactura(String factura) {
+        this.factura = factura;
+    }
+
+    public Cliente getComprador() {
+        return comprador;
+    }
+
+    public void setComprador(Cliente comprador) {
+        this.comprador = comprador;
+    }
+
+    public Compra(int codigo, String fecha, String factura, Cliente comprador) {
+        this.codigo = codigo;
+        this.fecha = fecha;
+        this.factura = factura;
+        this.comprador = comprador;
+    }
+
+    @Override
+    public String toString() {
+        return "Compra{" + "codigo=" + codigo + ", fecha=" + fecha + ", factura=" + factura + ", comprador=" + comprador + '}';
+    }
     
 }
